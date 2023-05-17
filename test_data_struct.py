41c7e37bf70f1164150abd51f116f8d831c7e9d5
justmehyes <valinocharlton@yahoo.ca>
2023-05-17 11:26:50 -0800

    cvbn

--- test_data_struct.py
+++ test_data_struct.py
@@ -230,8 +230,8 @@ if __name__ == "__main__":
     #test_struct("https://www.youtube.com/")
 
 
-    website_to_test = "https://foxnews.com"
-    seleniumsselector_list = ["refresh_sens:id;Fox News"]
+    website_to_test = "https://aol.ca"
+    seleniumsselector_list = ['css selector;div.hamburger-icon > svg']
     test_ss_list(website_to_test, seleniumsselector_list, c_driver)
     #test_struct("https://www.speedtest.net/")
 
